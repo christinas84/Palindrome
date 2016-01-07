4a4ff6fe801bc3ea62ba2fbd55be55e485913896
Christina <cesimmons84@gmail.com>
2016-01-07 11:28:20 -0500

    palindrome
    initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+ function palindrome(){
+        revString = "";
+        inpString = document.getElementById("text").value;
+        i = inpString.length;
+
+        for(var j=i; j>=0; j--){
+            revString = revString + inpString.charAt(j);
+        }
+        if(inpString === revString) {
+            document.getElementById("answer").innerHTML = inpString+" is a palindrome";
+        } else {
+            document.getElementById("answer").innerHTML = inpString+" is not a palindrome";
+        }
+ }
+   
+   function clearAnswer(){
+     document.getElementById("answer").innerHTML = '';
+   }
+
+    
